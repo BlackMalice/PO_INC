0a3eaa3b15395e13d377a54af4c3ccb22e47fd6c
BlackMalice <ergodam@mail.ru>
2022-06-18 02:38:39 +0200

    updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { url } = require("inspector");
 const app= express();
 const path= require("path");
 const port=3000;
-
+var device= require('express-device');
 //const company= require("./routes/company");
 app.use(express.static(__dirname+'/company'))
 app.use(express.static(__dirname+'/error'))
@@ -12,30 +12,55 @@ app.use(express.static(__dirname+'/policy'))
 //app.use("/company",company);
 app.use(express.static(__dirname+'/main'));
 app.use(express.static(__dirname +'/projects'))
+app.use(device.capture())
 
 
 app.get('/',(req,res)=>{
     
         
+    if(req.device.type=="desktop"){
+    res.sendFile(path.join(__dirname,'main/inc_main.html')); }
+    else {
+        res.sendFile(path.join(__dirname,'main/inc_mobile_main.html'));
+    }
     
-    res.sendFile(path.join(__dirname,'main/inc_main.html'))
-     
+ 
     
 });
 app.get('/company',(req,res)=>{
-    res.sendFile(path.join(__dirname,'company/inc_company.html'))
+    if(req.device.type=="desktop"){
+        res.sendFile(path.join(__dirname,'company/inc_company.html')); }
+        else {
+            res.sendFile(path.join(__dirname,'company/inc_mobile_company.html'));
+        }
 });
 app.get('/error',(req,res)=>{
-    res.sendFile(path.join(__dirname,'company/inc_error.html'))
+    if(req.device.type=="desktop"){
+        res.sendFile(path.join(__dirname,'error/inc_error.html')); }
+        else {
+            res.sendFile(path.join(__dirname,'errorinc_mobile_error.html'));
+        }
 });
 app.get('/service',(req,res)=>{
-    res.sendFile(path.join(__dirname,'service/inc_service.html'))
+    if(req.device.type=="desktop"){
+        res.sendFile(path.join(__dirname,'service/inc_service.html')); }
+        else {
+            res.sendFile(path.join(__dirname,'service/inc_mobile_service.html'));
+        }
 });
 app.get('/policy',(req,res)=>{
-    res.sendFile(path.join(__dirname,'policy/inc_policy.html'))
+    if(req.device.type=="desktop"){
+        res.sendFile(path.join(__dirname,'policy/inc_policy.html')); }
+        else {
+            res.sendFile(path.join(__dirname,'policy/inc_mobile_policy.html'));
+        }
 });
 app.get('/projects',(req,res)=>{
-    res.sendFile(path.join(__dirname,'projects/project.html'))
+    if(req.device.type=="desktop"){
+        res.sendFile(path.join(__dirname,'projects/inc_project.html')); }
+        else {
+            res.sendFile(path.join(__dirname,'projects/inc_mobile_project.html'));
+        }
 })
 
 
